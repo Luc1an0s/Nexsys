75868be4e75763a7740d2449c81700ccd24c6b82
Luciano Maciel <lucianomaciel53@gmail.com>
2025-05-25 21:29:08 -0400

    a

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-import {sql}
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+async function criarEPopularTabelaUsuarios(nome_completo, sobrenome, email, senha, data_cadastro, cpf, username, funcao) {
+    const db = await open({
+        filename: './banco.db',
+        driver: sqlite3.Database,
+    });
+    db.run(`CREATE TABLE IF NOT EXISTS usuarios 
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome_completo TEXT NOT NULL,
+        sobrenome TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        senha TEXT NOT NULL,
+        data_cadastro TEXT DEFAULT CURRENT_TIMESTAMP,
+        cpf TEXT NOT NULL UNIQUE,
+        username TEXT NOT NULL UNIQUE,
+        funcao TEXT NOT NULL);`);
+    // Aqui você pode adicionar o código para inserir usuários
+}
+
+criarEPopularTabelaUsuarios();
