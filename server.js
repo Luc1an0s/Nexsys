7cd86caba17a47a22c8548db895798e78eb95e3f
Luciano Maciel <lucianomaciel53@gmail.com>
2025-06-06 18:07:18 -0400

    a

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 import express from 'express';
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
-const users = []
+app.post('/usuarios', async (req, res) => {
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name: req.body.name,
+                email: req.body.email,
+                pass: req.body.password // campo correto conforme seu schema.prisma
+            }
+        });
+        res.status(201).send('Usuário criado com sucesso!');
+    } catch (error) {
+        res.status(400).send('Erro ao criar usuário: ' + error.message);
+    }
+});
 
-app.post('/usuarios', (req, res) => {
-
-    console.log(req.body);
-    
-    res.send('Usuário criado com sucesso!');
-})
-
-app.get('/usuarios', (req, res) => {
-    res.send('!!');
-})
-
-app.listen(3000)
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
 
 /* 
  - Criar um usuario
